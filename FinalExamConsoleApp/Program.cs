3e869d7f8ef82ed489510a113adc558c19732b3b
Mohammed Adem <mohata1234@gmail.com>
2020-11-19 20:15:01 -0500

    First commit

--- FinalExamConsoleApp/Program.cs
+++ FinalExamConsoleApp/Program.cs
@@ -2,11 +2,39 @@
 
 namespace FinalExamConsoleApp
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Adult a = new Adult();
+            a.firstname = "Mohammed";
+            a.lastname = "Adem";
+            child c = new child();
+            c.firstname = "Ali";
+            c.lastname = "Lake";
+            string[] myarray = new string[4];
+            myarray[0] = a.firstname;
+            myarray[1] = a.lastname;
+            myarray[2] = c.firstname;
+            myarray[3] = c.lastname;
+            for( int i=0;i<myarray.Length;i++)
+            {
+                Console.WriteLine(myarray[i]);
+            }
+
+
         }
     }
-}
+
+    public class Adult
+        {
+           public string firstname;
+           public string lastname ;
+        }
+    public class child
+        {
+            public string firstname;
+            public string lastname ;
+        }
+    }
+
